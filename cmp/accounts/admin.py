9a4463b3b1cec9b5fd15f6b8c88cc83fd96b70df
Gonzalo <gonzalo_a_fuente@hotmail.com>
2023-01-11 13:05:34 -0300

    Setup Database and make migrations

--- cmp/accounts/admin.py
+++ cmp/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
+
+# Change default headers
+
+admin.site.site_header = "CMP Admin"
+admin.site.site_title = "CMP Admin Area"
+admin.site.index_title = "Welcome to the Customer Management Platform Administration Area"
 
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Tag)
+admin.site.register(Order)
