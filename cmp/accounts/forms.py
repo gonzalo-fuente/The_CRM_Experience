5c6f973d0f21f4d0a846d27d4136e41c6b80fea9
Gonzalo <gonzalo_a_fuente@hotmail.com>
2023-01-13 03:23:14 -0300

    Add user login and registration

--- cmp/accounts/forms.py
+++ cmp/accounts/forms.py
@@ -1,8 +1,30 @@
-from django.forms import ModelForm
-from .models import Order
+from django.forms import ModelForm, TextInput, EmailInput, PasswordInput
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
 
+from .models import Order
 
 class OrderForm(ModelForm):
 	class Meta:
 		model = Order
 		fields = '__all__'
+
+class CreateUserForm(UserCreationForm):
+	password1 = forms.CharField(label="Password",
+		widget=forms.PasswordInput(
+			attrs={'class':'form-control form-control-md', 'type':'password', 'align':'center'}))
+	password2 = forms.CharField(label="Confirm password",
+		widget=forms.PasswordInput(
+			attrs={'class':'form-control form-control-md', 'type':'password', 'align':'center'}))
+
+	class Meta:
+		model = User
+		fields = ['username', 'email']
+		widgets = {
+            'username': TextInput(attrs={
+                'class': "form-control form-control-md"}),
+            'email': EmailInput(attrs={
+                'class': "form-control form-control-md"}),
+        }
+
