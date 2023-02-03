a3127973aa4025dab79eacc3c6506368318b445d
Gonzalo <gonzalo_a_fuente@hotmail.com>
2023-02-02 23:47:06 -0300

    Enable DEBUG to find redirection issue

--- cmp/settings.py
+++ cmp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = 1  # 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
